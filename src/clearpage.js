90e36260acb4d73765070e834801c66cc6962398
trung tran <nhoxgalai37@gmail.com>
2023-07-03 20:45:38 -0400

    Delete src directory

--- src/clearpage.js
+++
@@ -1,7 +0,0 @@
-export default function clearPage(e) {
-  const container = document.querySelector('#content');
-  const navbar = document.querySelector('#nav-bar');
-  while (container.lastChild !== navbar) {
-    container.removeChild(container.lastChild);
-  }
-}
